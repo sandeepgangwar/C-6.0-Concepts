24233c8f4128e2bfe97bca76917e5a6223579fb0
sandeep <sandeep.gangwar@silicus.com>
2017-07-25 12:34:14 +0530

    Added func and action

---
+++ FuncAndActionDelegates/MethodCollectios.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FuncAndActionDelegates
+{
+    public class MethodCollectios
+    {
+        public static void PrintText()
+        {
+            System.Console.WriteLine($"{nameof(PrintText)}");
+        }
+
+        public static void PrintNumbers(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                System.Console.WriteLine($"{nameof(PrintNumbers)}{i}");
+            }
+        }
+
+        public static void PrintMsg(string msg)
+        {
+            System.Console.WriteLine($"{nameof(PrintMsg)}:{msg}");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void DisplayAddition(int a, int b)
+        {
+            System.Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void ShowCompleteName(string fName, string lName)
+        {
+            System.Console.WriteLine($"Full Name {fName} {lName}");
+        }
+
+        public static int GetRandomNumber()
+        {
+            Random rd = new Random();
+            return rd.Next();
+        }
+    }
+}
---
+++ FuncAndActionDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FuncAndActionDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup
+            Action printText = new Action(MethodCollectios.PrintText);
+            Action<int, int> printNumber = new Action<int, int>(MethodCollectios.PrintNumbers);
+            Action<string> printMsg = new Action<string>(MethodCollectios.PrintMsg);
+            Action<string, string> showCompleteNumber = new Action<string, string>(MethodCollectios.ShowCompleteName);
+            Action<int, int> displayAddition = new Action<int, int>(MethodCollectios.DisplayAddition);
+            Console.WriteLine($"---Action<>----");
+            printText();
+            printNumber(1, 3);
+            printMsg("Hello from Main");
+            showCompleteNumber("Sandeep", "Singh");
+            displayAddition(11, 22);
+
+
+            //Setup
+            Func<int, int, int> addNumbers = new Func<int, int, int>(MethodCollectios.Add);
+            Func<int> generateRandomNumber = new Func<int>(MethodCollectios.GetRandomNumber);
+
+            Console.WriteLine($"---Func<>----");
+            Console.WriteLine(addNumbers(1, 2));
+            Console.WriteLine(generateRandomNumber());
+
+        }
+    }
+}
