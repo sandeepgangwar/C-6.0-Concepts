83d6517ed59a0d0abbb79f157357b46c7ebf0c36
sandeep <sandeep.gangwar@silicus.com>
2017-07-28 14:53:44 +0530

    Added webapi

---
+++ WebAPIBasics/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebAPIBasics.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "Hello", "World" };
+        }
+    }
+}
---
+++ WebAPIBasics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIBasics.WebApiApplication" Language="C#" %>
